998f39b00fc26f1b85ce7b3ab58f76a985b1d227
Anna Cho <loveablessing@gmail.com>
2022-11-13 02:43:01 -0500

    commit tree 1fbd067de4c01b82770a6ec7577e083e760d05be

--- looping.rb
+++ looping.rb
@@ -1,5 +1,11 @@
 def happy_new_year
   # your code here
+  counter = 10
+  until counter == 0
+    puts counter
+    counter -=  1
+  end
+    puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +23,26 @@ end
 
 def fizzbuzz_printer
   # your code here
+  (1..100).each do |num|
+    if num % 3 == 0 && num % 5 == 0
+      puts "FizzBuzz"
+    elsif num % 3 == 0
+      puts "Fizz"
+    elsif num % 5 == 0
+      puts "Buzz"
+    else
+      puts num
+    end
+  end
 end
 
 def reverse_string(str)
   # your code here
+  length = str.length - 1 
+  fizzbuzz = ""
+  until length == -1
+    fizzbuzz = fizzbuzz + str[length]
+    length -= 1
+  end
+  fizzbuzz
 end
